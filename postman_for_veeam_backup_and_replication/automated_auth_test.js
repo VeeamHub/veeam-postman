230a141c5a58131e2e21489387ded7c707d6297d
Chris Arceneaux <carcenea@gmail.com>
2021-02-05 16:09:58 -0500

    Added instructions for VBR 11 API
    Added documentation on how to import VBR API into Postman.
    
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

---
+++ postman_for_veeam_backup_and_replication/automated_auth_test.js
@@ -0,0 +1,16 @@
+//status code
+tests["Status code is 200"] = responseCode.code === 200;
+//access token
+pm.test("Access Token is present", function () {
+    pm.expect(pm.response.text()).to.include("access_token");
+});
+//updating global variable with token
+pm.test("Global Variable has been updated", function () {
+    var jsonData = pm.response.json();
+    pm.globals.set("vbr_access_token", jsonData.access_token);
+});
+//updating global variable with refresh token
+pm.test("Global Variable has been updated", function () {
+    var jsonData = pm.response.json();
+    pm.globals.set("vbr_refresh_token", jsonData.refresh_token);
+});
